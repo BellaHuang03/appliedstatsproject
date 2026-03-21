0991b656e363521ab3061821045c52ac15319682
Xia Meng Howey <xiameng.howey@duke.edu>
2026-03-20 21:26:35 -0400

    change tomato yield from 2014-2024 to 2013-2023

--- Tomato_yield_data.R
+++ Tomato_yield_data.R
@@ -6,21 +6,21 @@ library(tidyverse)
 tomato_df <- read.csv("CA_tomato_yield_by_county.csv", header = TRUE)
 
 #filter for 2014-2024 years
-tomato_df_2014to2024 <- tomato_df %>% 
-  filter(between(Year,2014,2024)) %>% 
+tomato_df_2013to2023 <- tomato_df %>% 
+  filter(between(Year,2013,2023)) %>% 
   filter(County != "OTHER (COMBINED) COUNTIES") %>% 
   filter(County != "OTHER COUNTIES") 
 
 #create figure
 #note this is for all available counties, we may filter out to target
 #certain counties
-fig1 <- ggplot(tomato_df_2014to2024,
+fig1 <- ggplot(tomato_df_2013to2023,
                aes(x = Year,
                    y = Value,
                    color = County))+
   geom_line()+
-  labs(title = "Tomato Yields in CA per county from 2014-2024",
-       caption = "Tomato Yields in CA per county from 2014-2024")
+  labs(title = "Tomato Yields in CA per county from 2013-2023",
+       caption = "Tomato Yields in CA per county from 2013-2023")
 fig1
 
 
